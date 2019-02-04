f0483e28051b0a54ea8dde76ab2950ec83c85d96
stevengangano <stevengangano@yahoo.com>
2019-02-03 20:19:46 -0800

    added video

--- db/migrate/20181210013411_add_user_id_to_songs.rb
+++
@@ -1,7 +0,0 @@
-# class AddUserIdToSongs < ActiveRecord::Migration
-#   def change
-#   	add_column :songs, :user_id, :integer
-#   end
-# end
-
-
--- db/migrate/20181210014356_create_songs.rb
+++ db/migrate/20181210014356_create_songs.rb
@@ -2,8 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
     	t.string :song
-
-      t.timestamps null: false
+      	t.timestamps null: false
     end
   end
 end
